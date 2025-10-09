958039d4a2b35f1ccd0bb66b98f9559b2f675206
SevvalElmaci <elmaci_sevval@hotmail.com>
2025-10-09 15:55:40 +0300

    10/09/2025

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PokemonReviewApp.Data;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
+using System.Security.Cryptography.X509Certificates;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace PokemonReviewApp.Controllers
@@ -27,11 +28,46 @@ namespace PokemonReviewApp.Controllers
         {
             var pokemons = _pokemonRepository.GetPokemons();
             if (!ModelState.IsValid)
-            {
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+
+        }
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound;
+
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if(!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-                return Ok(pokemons);
+            return Ok(pokemon);
+
             }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+
+            if (!_pokemonRepository.PokemonExists(pokeId))
+            return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(rating);
+
         }
+
     }
 }
+
--- PokemonReviewApp/Interfaces/IPokemonRepository.cs
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -11,5 +11,10 @@ namespace PokemonReviewApp.Interfaces
         //API CALL
 
         ICollection<Pokemon> GetPokemons(); 
+
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
     }
 }
